ba0d71ac8eff7b961094432dd1916020df0115d9
dal-dos <32851308+dal-dos@users.noreply.github.com>
2022-10-27 14:53:30 -0700

    Delete target/classes directory

--- target/classes/public/friendrequests.js
+++
@@ -1,9 +0,0 @@
-window.onload = myFunction();
-function myFunction() {
-    var otherusername1 = document.getElementById("otherusername1");
-    document.getElementById("nousers").style.display = "none";
-    if(!otherusername1){
-        document.getElementById("bigtable").style.display = "none";
-        document.getElementById("nousers").style.display = "block";
-    }
-}
